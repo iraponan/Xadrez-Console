2ccb59efa43fb49a72f0e4495c30b76cdec3f756
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-09 13:41:40 -0300

    Criando a classe Posição.

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,5 +1,11 @@
-﻿internal class Program {
+﻿using System.Security.Cryptography.X509Certificates;
+using Xadrez_Console.Tabuleiro;
+
+internal class Program {
     private static void Main(string[] args) {
-        Console.WriteLine("Hello, World!");
-    }
+        Posicao P = new Posicao(3, 4);
+
+        Console.WriteLine("Posição: " + P);
+        
+        }
 }
