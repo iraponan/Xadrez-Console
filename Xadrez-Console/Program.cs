97373f60b9206bedb3feaa8acbed0264135a49b8
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-09 14:12:04 -0300

    Começando a implementar a impressão do tabuleiro

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Security.Cryptography.X509Certificates;
+using Xadrez_Console;
 using Xadrez_Console.Tabuleiro;
 
 internal class Program {
     private static void Main(string[] args) {
         
         Tabuleiro tabuleiro = new Tabuleiro(8, 8);
-        }
+        Tela.imprimirTabuleiro(tabuleiro);
+    }
 }
---
+++ Xadrez-Console/Tela.cs
@@ -0,0 +1,19 @@
+﻿using Xadrez_Console.Tabuleiro;
+
+namespace Xadrez_Console {
+    internal class Tela {
+        public static void imprimirTabuleiro(Xadrez_Console.Tabuleiro.Tabuleiro tabuleiro) {
+            for (int i = 0; i < tabuleiro.linhas; i++) {
+                for (int j = 0; j < tabuleiro.colunas; j++) {
+                    if (tabuleiro.peca(i, j) == null) {
+                        Console.Write("- ");
+                    }
+                    else {
+                        Console.Write(tabuleiro.peca(i, j) + "");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
