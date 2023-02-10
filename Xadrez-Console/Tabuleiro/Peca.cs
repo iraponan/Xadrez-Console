1dd5de5d966ff11349ff4cf642a9395d3c9d238a
Iraponan Aires de Souza Marinho <iraponan.aires@outlook.com>
2023-02-10 15:11:59 -0300

    Correção: mudando o nome do metodo para movimentoPossivel

--- Xadrez-Console/Tabuleiro/Peca.cs
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -35,7 +35,7 @@
             return false;
         }
 
-        public bool podeMoverPara(Posicao posicao) {
+        public bool movimentoPossivel(Posicao posicao) {
             return movimentosPossiveis()[posicao.linha, posicao.coluna];
         }
     }
